4e277c30d5a5675fe74aad960b5c9f463ffde0b7
Ramadi Butarbutar <ramadisibutar05@gmail.com>
2023-08-16 17:25:59 +0700

    Input all file

---
+++ home.php
@@ -0,0 +1,27 @@
+
+<?php
+include('config.php');
+include('fungsi.php');
+
+// header
+include('header.php');
+?>
+
+	<section class="content">
+		<img src="img/longsor.jpg">
+			<h2 class="ui header">Kulon Progo</h2>
+
+			<p>Kabupaten Kulon Progo merupakan wilayah bagian Propinsi Daerah Istimewa Yogyakarta yang terletak paling barat dengan batas sebelah barat dan utara adalah Propinsi Jawa Tengah dan sebelah selatan adalah Samudera Indonesia . Secara geografis terletak antara 7 o 38'42" - 7 o 59'3" Lintang Selatan dan 110 o 1'37" - 110 o 16'26" Bujur Timur.
+
+			Luas area adalah 58.627,5 Ha yang meliputi 12 kecamatan dan 88 desa. Dari luas tersebut 24,89 % berada di wilayah Selatan yang meliputi kecamatan Temon, Wates, Panjatan dan Galur, 38,16 % di wilayah tengah yang meliputi kecamatan Lendah, Pengasih, Sentolo, Kokap, dan 36,97 % di wilayah utara yang meliputi kecamatan Girimulyo, Nanggulan, Kalibawang dan Samigaluh. Luas kecamatan antara 3.000 - 7.500 Ha dan yang wilayahnya paling luas adalah kecamatan Kokap seluas 7.379,95 Ha sedangkan yang wilayahnya paling sempit adalah kecamatan Wates seluas 3.200,239 Ha.
+			</p>
+
+			<p>Secara fisiografis kondisi Kabupaten Kulon Progo wilayahnya adalah daerah datar, meskipun dikelilingi pegunungan yang sebagian besar terletak pada wilayah utara, luas wilayahnya 17,58 % berada pada ketinggian < 7 m di atas permukaan laut, 15,20 % berada pada ketinggian 8 - 25 m di atas permukaan laut, 22,85 % berada pada ketinggian 26 - 100 m di atas permukaan laut, 33,00 % berada pada ketinggian 101 - 500 m di atas permukaan laut dan 11,37 % berada pada ketinggian > 500 m di atas permukaan laut. Jika dilihat letak kemiringannya, luas wilayahnya 58,81 % kemiringannya < 15° , 18,73 % kemiringannya antara 16° - 40° dan 22,46 % kemiringannya > 40°</p>
+
+			<p>Dengan kondisi seperti ini maka wilayah yang Kabupaten Kulon Progo sebagain besar rawan terjadinya longsor. Oleh sebab itu perlu dilakukan pengananganan sejak dini untuk menanggulangi terjadinya longsor dan untuk menghindari adanya korban bencana akibat terjadinya longsor</p>
+
+			<br>
+
+	</section>
+
+<?php include('footer.php'); ?>
